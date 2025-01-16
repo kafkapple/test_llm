b761e852dd2af88bd1a1962f7765304741d6b169
kafkapple <biasdrive@gmail.com>
2025-01-17 00:30:31 +0900

    fix: summary rep gen issue

--- main.py
+++ main.py
@@ -24,7 +24,23 @@ def main(cfg: DictConfig):
             pipeline.process(user_input)
     else:
         # Single task mode
-        input_text = """아 개짜증나, 시발 새끼. 나가 뒤져라. 씨발 개새끼야."""
+        input_text = """moser lab 의 grid cell 관련 연구, x y 축 반복되는 패턴 toroidal 
+
+어릴 때 눈 못뜰 때 recording, task 못하니
+grid cell 은 없는데, toroidal manifold 는 존재
+
+x y 축 2d space 에서 돌아다니니
+collection
+
+task 발생하기 전부터 manifold 발생한다. 
+mean? attractor 도 비슷하다 생각. 
+task dependent 하지 않은 그런 attractor 좀 더 멋지지 않나. 
+
+
+ersistent activity 를 유지하는 메커니즘으로 최근 Andermann 랩에서 주장하는 cAMP accumulation 이 여기서도 마찬가지로 적용될 수도 있을 것 같다는 생각이드네요. 저번에 지수님 sweet adaptation 도 cAMP accumulation 관련 메커니즘이었지 않았나요.
+
+camp 같은 2nd messenger 역할 중요할것으로 보임?"""
+        input_text = """근데, 아직 1년도 안됐어. 그리고 난 아직 제대로 뭔가 풀어낸 적도 없다고. 억울하고 화가나는데 젠장."""
         pipeline.process(input_text)
 
 if __name__ == "__main__":
